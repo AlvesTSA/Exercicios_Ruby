3a44d4620ebc28443766dd4383d4b743ba1e5fda
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-06 17:51:40 -0300

    exercicio_3

---
+++ Estrutura_Sequencial/exercicio_3.rb
@@ -0,0 +1,11 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+puts "Digite um número: "
+num = gets.chomp.to_f
+
+puts "Digite outro número: "
+num2 =gets.chomp.to_f
+
+soma = num + num2
+
+puts "A soma dos números é: #{soma}"
