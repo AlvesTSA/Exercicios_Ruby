5ef2ab4eda7a8c4f314f9e97f842bd61d9d9dae7
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-06 23:15:54 -0300

    exercicio_5

---
+++ Estrutura_Sequencial/exercicio_5.rb
@@ -0,0 +1,8 @@
+#Faça um Programa que converta metros para centímetros.
+
+    puts "Informe a uma medida em metros: "
+    metro = gets.chomp.to_f
+
+    centimetro = metro*100
+
+    puts "A medida em centimetro é: #{centimetro}"
