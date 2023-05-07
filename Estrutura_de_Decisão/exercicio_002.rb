45b57e3c736b02f1ae83386814a42ae1399afa49
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-05-07 16:57:38 -0300

    exercicio_002

---
+++ Estrutura_de_Decisão/exercicio_002.rb
@@ -0,0 +1,16 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+$valor = 0
+
+puts "Informe um valor: "
+valor = gets.to_i
+
+if valor > 0
+
+    puts "POSITIVO"
+
+else 
+
+    puts "NEGATIVO"
+
+end
