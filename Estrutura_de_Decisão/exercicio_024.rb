1136496a9cc16c72cd23ed7dded28823db25ebb7
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-06-26 19:54:55 -0300

    exercicio_24

---
+++ Estrutura_de_Decisão/exercicio_024.rb
@@ -0,0 +1,35 @@
+#Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário a valor do saque e depois informar quantas notas de cada valor serão fornecidas. As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar com a quantidade de notas existentes na máquina.
+
+#Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece duas notas de 100, uma nota de 50, uma nota de 5 e uma nota de 1;
+ 
+#Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
+
+saque = 0
+nota1 = 0
+nota5 = 0
+nota10 = 0
+nota50 = 0
+nota100 = 0
+
+puts "Informe quanto deseja sacar"
+saque = gets.chomp.to_i
+
+if saque > 0 && saque <= 600
+
+    nota100 = saque / 100
+    saque %= 100
+    nota50 = saque / 50
+    saque %= 50
+    nota10 = saque / 10
+    saque %= 10
+    nota5 = saque / 5
+    saque %= 5
+    nota1 = saque 
+
+else 
+    puts "Informe um valor de saque válido"
+
+    return
+end
+
+puts "Nota de 100: #{nota100}\nNota de 50: #{nota50}\nNota de 10: #{nota10}\nNota de 5: #{nota5}\nNota de 1: #{nota1}"
