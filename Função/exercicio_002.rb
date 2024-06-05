4162bb2bc39f862e8b7de2769cedfc72aecfb66b
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-06-05 09:08:13 -0300

    Create exercicio_002.rb

---
+++ Função/exercicio_002.rb
@@ -0,0 +1,15 @@
+"""/*2. Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.*/
+    """
+    
+def somaImposto(taxaImposto, custo)
+
+    return custo + (custo*(taxaImposto / 100))
+end
+
+print "Informe o a taxa de imposto: "
+taxaImposto = gets.chomp.to_f
+print "Informe o custo do preoduto: "
+custo = gets.chomp.to_f
+custo_final = somaImposto(taxaImposto,custo)
+
+print "Valor do produto com imposto: #{'%.2f'%custo_final}"
